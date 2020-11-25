d3b600400064602f21f5cf453e3eedf4f27e0a03
Jo You <67471883+pricklyCyuan@users.noreply.github.com>
2020-11-25 23:51:09 +0800

    Add files via upload

---
+++ Q1_1 line.py
@@ -0,0 +1,7 @@
+#像程式設計師這樣思考
+#Chapter 2 問題:一條線
+
+for hashnum in range(5):
+		print("#", end = '')	#因為python有自動換行的功能，限制end來避免患行	
+print('')						#print 一個空字串會自帶一個換行
+#print('\n', end = '')
---
+++ Q3_ triangle.py
@@ -0,0 +1,7 @@
+#像程式設計師這樣思考
+#Chapter 2 問題:倒直角三角形
+
+for raw in range(5):
+	for hashnum in range(1,6-raw,1):
+		print("#", end = '')	#因為python有自動換行的功能，限制end來避免患行
+	print('')					#print 一個空字串會自帶一個換行
