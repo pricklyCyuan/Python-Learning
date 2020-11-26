20a2281ea5e391a4e3cf296cccea836823e57425
Jo You <67471883+pricklyCyuan@users.noreply.github.com>
2020-11-26 21:58:18 +0800

    Update Q3_Triangle.py
    Typo

--- Q3_Triangle.py
+++ Q3_Triangle.py
@@ -3,5 +3,5 @@
 
 for raw in range(5):
 	for hashnum in range(0,5-raw,1):
-		print("#", end = '')	#因為python有自動換行的功能，限制end來避免患行
+		print("#", end = '')	#因為python有自動換行的功能，限制end來避免換行
 	print('')			#print 一個空字串會自帶一個換行
