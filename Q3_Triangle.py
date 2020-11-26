7082cfd55251b7bb0965f6adc4e8fdab11ba5c4f
Jo You <67471883+pricklyCyuan@users.noreply.github.com>
2020-11-26 21:57:02 +0800

    Update Q3_Triangle.py
    redefine the Range parameter in line 5.

--- Q3_Triangle.py
+++ Q3_Triangle.py
@@ -2,6 +2,6 @@
 #Chapter 2 問題:倒直角三角形
 
 for raw in range(5):
-	for hashnum in range(1,6-raw,1):
+	for hashnum in range(0,5-raw,1):
 		print("#", end = '')	#因為python有自動換行的功能，限制end來避免患行
 	print('')			#print 一個空字串會自帶一個換行
