17203a238c0574668a51b5388fffe0d44a4a6103
Jo You <67471883+pricklyCyuan@users.noreply.github.com>
2020-11-30 22:37:35 +0800

    Update Q5_Luhn Checksum Part 1.py

--- Q5_Luhn Checksum Part 1.py
+++ Q5_Luhn Checksum Part 1.py
@@ -1,7 +1,7 @@
 #像程式設計師這樣思考
 #Chapter 2 問題:Luhn Checksum 第一部分 大於10的數字處理
 
-digit = int(input('please enter a single digit number ,0-9 :'))
+digit = int(input('please enter a single digit number ,0-9 :')) #讀取輸入值 並轉換回int 型別
 
 if (digit*2>=10):
 	sum = 1 + (2*digit)%10
